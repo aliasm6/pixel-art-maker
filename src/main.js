45b2ae8b69d1ec0a93413753be8c6596e24c367b
Alias M6 <aliasmontoya719@gmail.com>
2016-07-21 13:16:19 -0600

    adding some styling removing index from src

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
 
   function newCanvas() {
-    for(var i = 0; i < 2100; i++ ) {
+    for(var i = 0; i < 1890; i++ ) {
       $('#canvas').append('<div class="pixel"></div>');
       console.log("test");
     }
@@ -18,7 +18,7 @@ $(document).ready(function () {
 
   })
   function newPallette() {
-    var colores = ['red','orange','yellow','green','blue','indigo','violet']
+    var colores = ['red','orange','yellow','green','blue','indigo','violet','white']
     for(var i = 0; i < colores.length; i++) {
     $('#pallette').append('<div class="styling '+colores[i]+'"></div>')
     $('.'+colores[i]).css('backgroundColor', colores[i]).width('40px').height('40px')
@@ -28,5 +28,20 @@ $(document).ready(function () {
   $('.styling').on('click', function(e){
     color = $(this).css('backgroundColor')
   })
+
+  var isDown = false;   // Tracks status of mouse button
+
+   $('#canvas > div').mousedown(function() {
+      isDown = true;      // When mouse goes down, set isDown to true
+    })
+   $('#canvas > div').mouseup(function() {
+      isDown = false;    // When mouse goes up, set isDown to false
+    });
+
+   $("#canvas > div").mouseover(function() {
+     if(isDown) {        // Only change css if mouse is down
+        $(this).css('background-color', color)
+     }
+    });
 });
 //branched
